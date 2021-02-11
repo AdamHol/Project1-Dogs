3ef307dc95cd10ff9d59b4670ccdaac2ac9d2b2f
Adam H <adam.holowieniec@gmail.com>
2021-02-11 12:32:39 +0000

    animate navbar menu in js (hide-show)

---
+++ main.js
@@ -0,0 +1,25 @@
+const burger = document.getElementById("burger");
+burger.addEventListener("click", showMenu);
+
+function showMenu() {
+  const burgerMenu = document.getElementById("burgerMenu");
+  console.log(burgerMenu.style.color);
+  // if (burgerMenu.style.transform === "translateX(800px)") {
+  burgerMenu.style.transform = "translateX(0px)";
+  // } else {
+  //   burgerMenu.style.transform = "translateX(800px)";
+  // }
+}
+
+const burgerClose = document.getElementById("closeMenu");
+burgerClose.addEventListener("click", hideMenu);
+
+function hideMenu() {
+  const hideMenu = document.getElementById("burgerMenu");
+  console.log(hideMenu.style.color);
+  // if (hideMenu.style.transform === "translateX(800px)") {
+  // hideMenu.style.transform = "translateX(0px)";
+  // } else {
+  hideMenu.style.transform = "translateX(800px)";
+  // }
+}
